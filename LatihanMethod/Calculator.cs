524d5d4fafb653efe8d0f360c4c4e1b51252c607
anksite <adnankurniawan98@gmail.com>
2019-04-02 14:55:51 +0700

    commit awal

---
+++ LatihanMethod/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod {
+
+    class Calculator {
+
+        public int Penjumlahan(int a, int b) {
+            return a + b;
+        }
+
+        public int Penjumlahan(int a, int b, int c) {
+            return a + b + c;
+        }
+
+        public int Pengurangan(int a, int b) {
+            return a - b;
+        }
+
+        //method static
+        /// <summary>
+        /// Static method perkalian
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Perkalian(int a, int b) {
+            return a * b;
+        }
+    }
+}
---
+++ LatihanMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod {
+
+    class Program {
+
+        static void Main(string[] args) {
+            int hasil;
+
+            Calculator calc = new Calculator();
+
+            hasil = calc.Penjumlahan(10, 2);
+            cetakHasil("Hasil penjumlaham: "+ hasil);
+
+            hasil = calc.Penjumlahan(10, 2, 3);
+            cetakHasil("Hasil penjumlaham 3 bilangan: " + hasil);
+
+            hasil = calc.Pengurangan(7, 2);
+            cetakHasil("Hasil Pengurangan: " + hasil);
+
+            hasil = Calculator.Perkalian(5, 2);
+            cetakHasil("Hasil Perkalian: " + hasil);
+
+            Console.ReadKey();
+        }
+
+        static void cetakHasil(string output) {
+            Console.WriteLine(output);
+        }
+    }
+}
